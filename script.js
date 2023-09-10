cf4608677057375c5516dc46b32551cb52344be7
Silantiev Maksim <silantiievmax@gmail.com>
2023-09-10 13:52:33 +0300

    add new comment

--- script.js
+++ script.js
@@ -1,49 +1,77 @@
-const data = document.querySelectorAll("[data-discussion-id]");
-const btns = document.querySelectorAll(".js-vue-toggle-button");
-const comment = document.querySelectorAll("[data-testid='thread-toggle']");
-
+let data = document.querySelectorAll("[data-discussion-id]");
 const nameStor = data[0].dataset.discussionId;
-const arrDataSet = [];
 
-const getLocalStoreg = JSON.parse(window.localStorage.getItem(nameStor)) ?? [];
+let getLocalStoreg = JSON.parse(window.localStorage.getItem(nameStor)) ?? [];
 
-if (getLocalStoreg.length > 0) {
-  comment.forEach((item, id) => {
-    const element = getLocalStoreg[id];
+const setItemLocalStorage = (dataId, name) => {
+  const newDataArr = [];
 
-    if (element.flag) {
-      const colorElement = item.closest(".timeline-content");
-      colorElement.style.borderColor = "#be29ff";
-      colorElement.style.color = "#be29ff";
-      item.click();
-    }
-  });
-} else {
-  data.forEach((item) => {
-    const dataset = item.dataset.discussionId;
-
-    arrDataSet.push({ dataset: dataset, flag: false });
+  dataId.forEach((item) => {
+    newDataArr.push({ dataset: item.dataset.discussionId, flag: false });
   });
 
-  window.localStorage.setItem(nameStor, JSON.stringify(arrDataSet));
-}
+  window.localStorage.setItem(name, JSON.stringify(newDataArr));
+};
 
-btns.forEach((btn) => {
-  const dataSet = btn.closest("[data-discussion-id]");
+const toggleButton = (name) => {
+  const toggleButtons = document.querySelectorAll(".js-vue-toggle-button");
 
-  btn.addEventListener("click", (e) => {
-    const newDataset = dataSet.dataset.discussionId;
-    const newArr = JSON.parse(window.localStorage.getItem(nameStor));
+  toggleButtons.forEach((btn) => {
+    const dataDiscussionId = btn.closest("[data-discussion-id]");
 
-    newArr.filter((e) => {
-      if (e.dataset === newDataset) {
-        e.flag = !e.flag;
-      }
+    btn.addEventListener("click", (e) => {
+      const getDataset = dataDiscussionId.dataset.discussionId;
+      const newArr = JSON.parse(window.localStorage.getItem(name));
 
-      window.localStorage.setItem(nameStor, JSON.stringify(newArr));
+      newArr.filter((e) => {
+        if (e.dataset === getDataset) {
+          e.flag = !e.flag;
+        }
+      });
+
+      window.localStorage.setItem(name, JSON.stringify(newArr));
     });
   });
-});
+};
+
+const closeComment = (dataId, name) => {
+  const comments = document.querySelectorAll("[data-testid='thread-toggle']");
+  getLocalStoreg = JSON.parse(window.localStorage.getItem(name));
+
+  comments.forEach((coment, id) => {
+    const element = dataId[id];
+
+    if (element?.flag) {
+      const colorElement = coment.closest(".timeline-content");
+      colorElement.style.borderColor = "#be29ff";
+      colorElement.style.color = "#be29ff";
+      coment.click();
+    }
+  });
+};
+
+const arrDataSet = [];
+
+if (getLocalStoreg.length > 0) {
+  if (getLocalStoreg.length < data.length) {
+    getLocalStoreg = JSON.parse(window.localStorage.getItem(nameStor));
+
+    data.forEach((item, key) => {
+      if (item.dataset.discussionId === getLocalStoreg[key]?.dataset) {
+        arrDataSet.push(getLocalStoreg[key]);
+      } else {
+        arrDataSet.push({ dataset: item.dataset.discussionId, flag: false });
+      }
+      window.localStorage.setItem(nameStor, JSON.stringify(arrDataSet));
+    });
+  }
+
+  closeComment(getLocalStoreg, nameStor);
+} else {
+  setItemLocalStorage(data, nameStor);
+}
+
+toggleButton(nameStor);
 
 console.log("%cSilantevMax", "color:#233E82; font-family:'Ubuntu'; display: block;font-weight:bold; font-size:48px;");
 console.log("%cНе обращай внимания на мой код, я джун и писал его 15 минут", "color:#233E82; font-family:'Ubuntu'; font-weight:100; font-size:24px;");
